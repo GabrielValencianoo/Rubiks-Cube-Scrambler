13b06e55c8352076c8a3e33678828a125b4a31bb
Valenciano13 <gabriel.valenciano7@gmail.com>
2025-01-26 01:05:23 -0300

    Added Ao5 in csv
    Added Ao5 in csv to ease the use external

--- scrambler.py
+++ scrambler.py
@@ -4904,6 +4904,7 @@ def importar_tempos_file():
                 datas.append(row["Date"])
                 status.append(row["Status"])
                 inputs.append(row["Input"])
+                # ao_5.append(row["Ao5"])
                 
                 estatistica(index)                    
 
@@ -4934,6 +4935,7 @@ def importar_tempos_folder():
                     datas.append(row["Date"])
                     status.append(row["Status"])
                     inputs.append(row["Input"])
+                    # ao_5.append(row["Ao5"])
                     
                     estatistica(index)                    
 
@@ -4951,10 +4953,10 @@ def exportar_tempos():
     with open(name_file, mode='w') as employee_file:
         employee_writer = csv.writer(employee_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-        employee_writer.writerow(["No.", "Time", "Inspection","Scramble","Date","Status","Input"])
+        employee_writer.writerow(["No.", "Time", "Inspection","Scramble","Date","Status","Input","Ao5"])
 
         for i in range(len(tempos)):
-            employee_writer.writerow([i+1, tempos[i], scrambles[i], datas[i],status[i],inputs[i]])
+            employee_writer.writerow([i+1, tempos[i], scrambles[i], datas[i],status[i],inputs[i],ao_5[i]])
 
     messagebox.showinfo( "Warning", "Export completo.")
 
@@ -4966,7 +4968,7 @@ def clear_file_tempos():
     with open(name_file, mode='w') as employee_file:
         employee_writer = csv.writer(employee_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-        employee_writer.writerow(["No.", "Time", "Inspection","Scramble","Date","Status","Input"])
+        employee_writer.writerow(["No.", "Time", "Inspection","Scramble","Date","Status","Input","Ao5"])
 
 def update_file_tempos(index):    
     name_file = eventsComboBox.get()  
@@ -4976,7 +4978,7 @@ def update_file_tempos(index):
         with open(name_file, mode='a') as employee_file:
             employee_writer = csv.writer(employee_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-            employee_writer.writerow([index, tempos[index-1],countdowns[index-1], scrambles[index-1], datas[index-1],status[index-1],inputs[index-1]])
+            employee_writer.writerow([index, tempos[index-1],countdowns[index-1], scrambles[index-1], datas[index-1],status[index-1],inputs[index-1],ao_5[index-1]])
             # print("insert feito")
 
     except Exception as error: 
@@ -4989,7 +4991,7 @@ def guardar_tempos():
         name_file = event + '.csv' 
         with open(name_file, mode='w') as employee_file:
             employee_writer = csv.writer(employee_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            employee_writer.writerow(["No.", "Time", "Inspection","Scramble","Date","Status","Input"])
+            employee_writer.writerow(["No.", "Time", "Inspection","Scramble","Date","Status","Input","Ao5"])
             
             if event == eventsComboBox.get():
                 for i in range(len(tempos)):
